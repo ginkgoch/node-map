92dd511604430ed995582927860727b9f1e520de
Ginkgoch <ginkgoch@outlook.com>
2019-08-10 21:55:26 +0800

    Implement ShapefileFeatureSource.

--- src/layers/Field.ts
+++ src/layers/Field.ts
@@ -2,10 +2,22 @@ export class Field {
     name: string;
     type: string;
     length: number;
+    extra: Map<string, any>;
 
-    constructor(name: string, type: string, length: number) {
+    constructor(name: string, type: string, length: number, extra?: any) {
         this.name = name;
         this.type = type;
         this.length = length;
+        this.extra = new Map();
+
+        if (extra instanceof Map) {
+            extra.forEach((v, k) => {
+                this.extra.set(k, v);
+            });
+        } else if (extra !== undefined) {
+            Object.keys(extra).forEach(k => {
+                this.extra.set(k, extra[k]);
+            });
+        }
     }
 }
