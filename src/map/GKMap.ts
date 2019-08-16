d23883db83d2375e507794e73d7642da5a4e75df
Ginkgoch <ginkgoch@outlook.com>
2019-08-16 17:19:55 +0800

    Add map json and tests.

--- src/map/GKMap.ts
+++ src/map/GKMap.ts
@@ -2,14 +2,15 @@ import _ from "lodash";
 import { IEnvelope, Envelope } from "ginkgoch-geom";
 import { Render } from "../render";
 import { LayerGroup, FeatureLayer, Srs } from "../layers";
+import { Constants } from "../shared";
 
 export class GKMap {
     name = 'GKMap';
-    srs?: Srs;
+    srs: Srs;
     width: number;
     height: number;
     background?: string;
-    maximumScale = Number.POSITIVE_INFINITY;
+    maximumScale = Constants.POSITIVE_INFINITY_SCALE;
     minimumScale = 0;
     groups: Array<LayerGroup>;
 
@@ -18,6 +19,40 @@ export class GKMap {
         this.height = height || 256;
         this.srs = new Srs(srs || 'EPSG:3857');
         this.groups = new Array<LayerGroup>();
+    }
+
+    toJSON() {
+        const json: any = {
+            name: this.name,
+            srs: this.srs.toJSON(),
+            width: this.width,
+            height: this.height,
+            maximumScale: this.maximumScale,
+            minimumScale: this.minimumScale,
+            groups: this.groups.map(g => g.toJSON())
+        };
+
+        if (this.background) {
+            json.background = this.background;
+        }
+
+        return json;
+    }
+
+    static parseJSON(json: any) {
+        const map = new GKMap();
+        map.name = json.name;
+        map.srs = Srs.parseJSON(json.srs);
+        map.width = json.width;
+        map.height = json.height;
+        map.maximumScale = json.maximumScale;
+        map.minimumScale = json.minimumScale;
+        map.groups = (<any[]>json.groups).map(g => LayerGroup.parseJSON(g));
+        if (json.background !== undefined) {
+            map.background = json.background;
+        }
+
+        return map;
     }
 
     async envelope() {
