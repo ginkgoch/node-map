bbb3b4325d7443c82fbefaa4e1ed800e428ff56f
Ginkgoch <ginkgoch@outlook.com>
2019-08-18 11:13:17 +0800

    Refine export index.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
+
+export * from './layers';
+export * from './render';
+export * from './shared';
+export * from './styles';
+export * from './native';
+export * from './map';
+
 import * as layers from './layers';
 import * as render from './render';
 import * as shared from './shared';
 import * as styles from './styles';
 import * as native from './native';
-import * as map from './map';
-
-const mapkit = {
+import * as mapping from './map';
+export default {
     layers,
     render,
     shared,
     styles,
     native,
-    map
+    mapping
 };
-
-export default mapkit;
