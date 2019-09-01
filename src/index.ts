06241548a2322c10612315df0f88220b6d8fc97c
Ginkgoch <ginkgoch@outlook.com>
2019-09-01 11:45:32 +0800

    Export indices.

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from './shared';
 export * from './styles';
 export * from './native';
 export * from './map';
+export * from './indices';
 
 import * as layers from './layers';
 import * as render from './render';
@@ -12,11 +13,14 @@ import * as shared from './shared';
 import * as styles from './styles';
 import * as native from './native';
 import * as mapping from './map';
+import * as indices from './indices';
+
 export default {
     layers,
     render,
     shared,
     styles,
     native,
-    mapping
+    mapping,
+    indices
 };
