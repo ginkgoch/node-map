60417e2e048628ab41da93a1611bdf78ea5fd3a8
Ginkgoch <ginkgoch@outlook.com>
2019-09-07 21:03:01 +0800

    162: ease api for engine.

--- tests/map/MapEngine.test.ts
+++ tests/map/MapEngine.test.ts
@@ -2,6 +2,7 @@ import { ShapefileFeatureSource, FeatureLayer } from '..';
 import { MapEngine } from '../../src/map';
 import { FillStyle } from '../../src/styles';
 import TestUtils from '../shared/TestUtils';
+import { LayerGroup, MemoryFeatureSource } from '../../src/layers';
 
 const compareImage = TestUtils.compareImageFunc(name => './tests/data/map/' + name);
 
@@ -14,6 +15,33 @@ describe('Map', () => {
         map = new MapEngine(256, 256, 'WGS84');
         expect(map.srs.unit).toEqual('degrees');
     });
+
+    it('push groups', () => {
+        let map = new MapEngine();
+        map.pushGroups(new LayerGroup(), new LayerGroup());
+        expect(map.groups.length).toBe(2);
+    });
+
+    it('get layer', () => {
+        let map = new MapEngine();
+        const layer1 = new FeatureLayer(new MemoryFeatureSource());
+        layer1.name = 'layer1';
+        map.pushLayers([layer1]);
+
+        const layer2 = new FeatureLayer(new MemoryFeatureSource());
+        layer2.name = 'layer1';
+        map.pushLayers([layer2], 'OL2');
+
+        const layer1_1 = map.layer('layer1', 'Default');
+        expect(layer1_1).not.toBeUndefined();
+        expect(layer1_1).toBe(layer1);
+
+        const layer1_2 = map.layer('layer1', 'OL2');
+        expect(layer1_2).toBe(layer2);
+
+        const layer1_3 = map.layer('layer1', 'OL3');
+        expect(layer1_3).toBeUndefined();
+    }); 
 
     it('draw', async () => {
         const map = getMap();
