065e4016ea71b45deacb1d7280d92468bd64e302
Ginkgoch <ginkgoch@outlook.com>
2020-02-20 17:02:59 +0800

    Improve text style mess up for invalid sources.

--- src/layers/MultiSourceFeatureLayer.ts
+++ src/layers/MultiSourceFeatureLayer.ts
@@ -108,7 +108,9 @@ export class MultiSourceFeatureLayer extends Layer {
         
         const fields = _.chain(styles).flatMap(s => s.fields()).uniq().value();
         for (let source of this.sources) {
-            const features = await source.features(envelope, fields);
+            const currentFields = (await source.fields()).map(f => f.name);
+            const drawingFields = _.intersection(fields, currentFields);
+            const features = await source.features(envelope, drawingFields);
             styles.forEach(style => {
                 style.drawAll(features, render);
             });
